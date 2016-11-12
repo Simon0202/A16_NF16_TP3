1814b2e3f118cb812f7b90f8e57158a1d8e0ecd8
Simon LAURENT <simon.laurent@gmx.com>
2016-11-12 23:30:56 +0100

    travail en cours pour fixer les bugs sur les ajouts des produits

--- TP3/TP3/tp3.c
+++ TP3/TP3/tp3.c
@@ -42,16 +42,38 @@ T_Magasin *creerMagasin(char *nom)
 }
 
 void menu(){
+    //*********************************
     //Table d'initialisation d'une base
+    //*********************************
+    
+    //Magasin
     T_Magasin *firstMagasin = creerMagasin("Carrefour");
+    
+    //Premier Rayon
     T_Rayon *milk = creerRayon("milk");
     ajouterRayon(firstMagasin, milk);
     ajouterProduit(milk, creerProduit("danone", 1.25, A, 200));
     ajouterProduit(milk, creerProduit("actimel", 2, A, 1200));
     ajouterProduit(milk, creerProduit("fromage", 1.25, C, 20));
-    //ajouterProduit(milk, creerProduit("lait", 1.25, B, 78));
-    //T_Rayon *meat = creerRayon("meat");
-
+    ajouterProduit(milk, creerProduit("lait", 1.02, B, 78));
+    
+    //Deuxieme Rayon
+    T_Rayon *meat = creerRayon("meat");
+    ajouterRayon(firstMagasin, meat);
+    ajouterProduit(meat, creerProduit("boeuf", 4, A, 100));
+    ajouterProduit(meat, creerProduit("poulet", 2, B, 120));
+    ajouterProduit(meat, creerProduit("dinde", 3.12, C, 57));
+    ajouterProduit(meat, creerProduit("mouton", 8, A, 90));
+    
+    
+    //Troisieme Rayon
+    T_Rayon *drink = creerRayon("drink");
+    ajouterRayon(firstMagasin, drink);
+    ajouterProduit(drink, creerProduit("coca", 1.2, A, 1000));
+    ajouterProduit(drink, creerProduit("fanta", 3.4, A, 1200));
+    ajouterProduit(drink, creerProduit("orangina", 3.12, A, 5700));
+    ajouterProduit(drink, creerProduit("ice tea", 5, A, 900));
+    
     
     
     
@@ -158,28 +180,47 @@ void menu(){
 
 
 //Q2
+//Les rayons sont triés par ordre croissant sur le nom des rayons
 int ajouterRayon(T_Magasin *magasin, T_Rayon *rayon){
     //Recuperation du magasin afin de ne pas perdre l'adresse du magasin
     //Il comprend la liste simplement chainée des rayons
     T_Magasin *currentMagasin = magasin;
     
     //On verifie qu'un magasin existe
-    if(currentMagasin==NULL){
-        printf("Il n'y a pas de magasin, on ne peut ajouter le rayon\n");
+    if(currentMagasin==NULL||rayon==NULL){
+        printf("Il n'y a pas de magasin ou de rayon, on ne peut ajouter le rayon\n");
         return 0;
     }
     //On verifie si le magasin contient dejà des rayons
-    if(currentMagasin->premier == NULL){
+    else if(currentMagasin->premier == NULL){
         currentMagasin->premier = rayon;
         return 1;
     }
+    //On verifie que le premier rayon ne porte pas le meme nom
+    else if(strcmp(currentMagasin->premier->nom_rayon,rayon->nom_rayon)==0){
+        printf("Il existe déjà un rayon se nommant: %s",rayon->nom_rayon);
+        return 0;
+    }
     else{
         //Sinon on va parcourir la liste simplement chainées
-        while(currentMagasin->premier->suivant!=NULL && strcmp(currentMagasin->premier->suivant->nom_rayon,rayon->nom_rayon)){
+        while(currentMagasin->premier->suivant!=NULL && strcmp(currentMagasin->premier->suivant->nom_rayon,rayon->nom_rayon)<=0 && strcmp(currentMagasin->premier->nom_rayon, rayon->nom_rayon)>0){
             currentMagasin->premier = currentMagasin->premier->suivant;
         }
+        if(currentMagasin->premier->suivant==NULL){
+            if(strcmp(currentMagasin->premier->nom_rayon,rayon->nom_rayon)>0){
+                rayon->suivant = currentMagasin->premier;
+                magasin->premier = rayon;
+                return 1;
+            }
+            else{
+                currentMagasin->premier->suivant=rayon;
+                return 1;
+            }
+        }
+        else{
         currentMagasin->premier->suivant = rayon;
         return 1;
+        }
     }
 }
 
@@ -199,8 +240,10 @@ int ajouterProduit(T_Rayon *rayon, T_Produit *produit){
         rayon->nombre_produit=produit->quantite_en_stock; //On suppose que le nombre de produit signifie le nombre de produit dans le rayon et non pas le nombre de reference de produit
         return 1;
     }
-    else if(strcmp(currentRayon->premier->marque,produit->marque) == 0)    //la même marque de produit dans un rayon
+    else if(strcmp(currentRayon->premier->marque,produit->marque) == 0){   //la même marque de produit dans un rayon
+        printf("un produit avec la marque %s existe dejà",produit->marque);
         return 0;
+    }
     //le produit ajoute a un prix plus petit que le celui en tete de liste
     else if(currentRayon->premier->prix >= produit->prix) {
         produit->suivant = currentRayon->premier;   //ajouter dans le premier
@@ -209,7 +252,7 @@ int ajouterProduit(T_Rayon *rayon, T_Produit *produit){
         return 1;
     }
     else{
-        while((currentRayon->premier->suivant != NULL) && (currentRayon->premier->suivant->prix < produit->prix))  //pas de meme marque et tier
+        while((currentRayon->premier->suivant != NULL) && (currentRayon->premier->suivant->prix < produit->prix) && (currentRayon->premier->prix <= produit->prix)) //pas de meme marque et trier
             currentRayon->premier = currentRayon->premier->suivant;
         
         if(currentRayon->premier->suivant == NULL){
